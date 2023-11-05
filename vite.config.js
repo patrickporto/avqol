87bab99e954547ff18458fc0c30583f84afee585
Patrick Porto <patrick.s.porto@gmail.com>
2023-11-05 11:47:55 -0700

    chore: replace caddy to vite https

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,40 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import pkg from "./package.json"
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
     root: 'src/',
     publicDir: resolve(__dirname, 'public'),
     base: `/modules/${pkg.name}/`,
     server: {
-      port: 30001,
-      open: '/game',
-      proxy: {
-        [`^(?!/modules/${pkg.name}/)`]: 'http://localhost:30000',
-        '/socket.io': {
-          target: 'ws://localhost:30000',
-          ws: true,
+        port: 30001,
+        open: '/game',
+        proxy: {
+            [`^(?!/modules/${pkg.name}/)`]: 'http://localhost:30000',
+            '/socket.io': {
+                target: 'ws://localhost:30000',
+                ws: true,
+            },
         },
-      }
+        https: true
     },
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    sourcemap: true,
-    lib: {
-      entry: './main.ts',
-      name: pkg.name,
-      formats: ['es'],
-      fileName: pkg.name,
+    plugins: [ mkcert() ],
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
+        sourcemap: true,
+        lib: {
+            entry: './main.ts',
+            name: pkg.name,
+            formats: ['es'],
+            fileName: pkg.name,
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: `${pkg.name}.[ext]`,
+            },
+        },
     },
-    rollupOptions: {
-      output: {
-        assetFileNames: `${pkg.name}.[ext]`,
-      },
-    },
-  },
 })
 
